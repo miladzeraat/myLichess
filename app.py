14c2603cafa8af3de80e0cd0c997af4222f555d3
miladzeraat <mi.zeraat@gmail.com>
2024-02-21 12:41:43 +0100

    submitting  username

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
-
+import time
 app = Flask(__name__)
 
 API_TOKEN = 'lip_yBjKhwsBvqUCJEu2Khcr'
 
 def get_player_info(username):
+    
     url = f'https://lichess.org/api/user/{username}'
     headers = {'Authorization': f'Bearer {API_TOKEN}'}
     
     try:
+        
         response = requests.get(url, headers=headers)
         response.raise_for_status()  # Raise an error for bad status codes (e.g., 404, 500)
         player_info = response.json()
@@ -18,10 +20,22 @@ def get_player_info(username):
         print(f"Error fetching player info: {e}")
         return None
 
-@app.route('/')
+
+@app.route('/', methods=['GET', 'POST'])
 def player():
-    player_info = get_player_info("miladonya")
-    return render_template('player.html', player_info=player_info)
+    
+    if request.method == 'POST':
+        username = request.form['username']
+        
+        player_info = get_player_info(username)
+        if player_info:
+            return render_template('player.html', player_info=player_info)
+        else:
+            return render_template('index.html', error="Failed to retrieve player information.")
+    else:
+        # Handle GET request
+        
+        return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(port=8090,debug=True)
